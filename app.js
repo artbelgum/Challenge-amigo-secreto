098430036710c1d1b5b037f0f666aa92b8b6e02a
artbelgum <artbelgum22@gmail.com>
2025-03-15 14:46:32 -0600

    funcion insertar nombres

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Creación de array de lista de amigos
+let amigos = [];
+
+/* Capturar el valor del campo de entrada */
+function agregarAmigo() {
+  let amigo = document.getElementById("amigo").value;
+  amigos.push(amigo);
+  document.getElementById("amigo").value = "";
+  console.log(amigos);
+}
+
+/* Función para mostrar el resultado */
+function mostrarResultado() {
+  let resultado = document.getElementById("resultado");
+  let amigosOrdenados = amigos.sort();
+  resultado.innerHTML = amigosOrdenados.join(", ");
+}
